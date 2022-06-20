b788477a51a99171f60eaa6a4a7806ad23df5534
JM <jesuscamposmendoza815@gmail.com>
2022-06-19 20:21:12 -0500

    11 added evensBetweenNumbers

---
+++ js-logic-practice/11-evenNumberBetweenN.js
@@ -0,0 +1,18 @@
+/*
+Numeros pares enntre dos numero
+*/
+
+function evens(n1,n2) {
+    if (n1 > n2 || (n1 && n2) < 1) {
+        console.log(`El primer parámetro debe ser menor que el segundo`);
+        console.log('Los numeros deben ser positivos y mayores a 0');
+    } else {
+        for (let i = n1; i <= n2; i++) {
+            (i % 2 == 0)
+            ? console.log('Even: ' + i)
+            : console.log('😔 Not even ' + i);
+        }
+    }
+}
+
+evens(0,20)
